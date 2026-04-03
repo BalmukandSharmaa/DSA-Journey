c7d472b8c06ead8fd865d75e390f94aa7b303046
Balmukand Sharma <143794800+BalmukandSharmaa@users.noreply.github.com>
2026-04-03 18:35:16 +0530

    Add: reverse-string - submission-1

---
+++ Data Structures & Algorithms/reverse-string/submission-1.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int x=0,y=s.size()-1;
+        while(x<y){
+            swap(s[x],s[y]);
+            x++;
+            y--;
+        }
+    }
+};
